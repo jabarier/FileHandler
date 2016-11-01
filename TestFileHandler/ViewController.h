4eca8285bfbf4cef735093a246721b5ebfefaec3
juwenz <juwenz@qq.com>
2016-11-01 09:55:27 +0800

    Initial Commit

---
+++ TestFileHandler/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestFileHandler
+//
+//  Created by Juwencheng on 2016/11/1.
+//  Copyright © 2016年 scics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestFileHandler/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestFileHandler
+//
+//  Created by Juwencheng on 2016/11/1.
+//  Copyright © 2016年 scics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
